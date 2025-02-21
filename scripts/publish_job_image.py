ece87f97f617e6e797010f935fb5d28e4913a483
Igor Bari <mucsi96@gmail.com>
2025-02-21 21:00:43 +0100

    Refactor Docker image publishing process and update Python version

--- scripts/publish_job_image.py
+++ scripts/publish_job_image.py
@@ -14,25 +14,12 @@ if username == None:
     print("GitHub username is missing", flush=True, file=sys.stderr)
     exit(1)
 
-server_version = version_utils.get_version(
-    src=root_directory, ignore=[str(root_directory / 'backup-job')], tag_prefix="server")
-
-docker_utils.build_and_push_docker_img(
-    src=root_directory,
-    version=server_version,
-    tag_prefix="server",
-    image_name="postgres-azure-backup",
-    docker_username=username,
-    docker_password=secrets["docker_password"],
-    github_access_token=sys.argv[1],
-)
-
-job_version = version_utils.get_version(
+version = version_utils.get_version(
     src=root_directory / 'backup-job', tag_prefix="job")
 
 docker_utils.build_and_push_docker_img(
     src=root_directory / 'backup-job',
-    version=job_version,
+    version=version,
     tag_prefix="job",
     image_name="postgres-azure-backup-job",
     docker_username=username,
