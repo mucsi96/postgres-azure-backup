7a3a463895c8106ac2cba549cee4fb69a819124c
Igor Bari <mucsi96@gmail.com>
2025-03-01 12:32:21 +0100

    Retrieve hostname from Azure Key Vault in deploy job script

--- scripts/deploy_job.sh
+++ scripts/deploy_job.sh
@@ -2,6 +2,7 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
+host=$(az keyvault secret show --vault-name p05 --name hostname --query value --output tsv)
 apiClientId=$(az keyvault secret show --vault-name p05 --name backup-api-client-id --query value --output tsv)
 cronJobClientId=$(az keyvault secret show --vault-name p05 --name backup-cron-job-client-id --query value --output tsv)
 latestTag=$(curl -s "https://registry.hub.docker.com/v2/repositories/mucsi96/postgres-azure-backup-job/tags/" | jq -r '.results |  map(select(.name != "latest")) | sort_by(.last_updated) | reverse | .[0].name')
