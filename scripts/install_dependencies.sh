29da0d0b49ddda30a56e01800fd993d05aea01e2
Igor Bari <mucsi96@gmail.com>
2025-10-20 13:48:40 +0200

    chore: update task commands to use bash and add args for scripts chore: update mock service worker version and improve request handling fix: change shebang in install_dependencies.sh to bash and activate virtual environment

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e  # Exit immediately if a command exits with a non-zero status
+
+source .venv/bin/activate
 
 storageAccountBlobUrl=$(az storage account show --name ibari --resource-group ibari --query "primaryEndpoints.blob" --output tsv)
 spaClientId=$(az keyvault secret show --vault-name p06 --name backup-spa-client-id --query value -o tsv)
@@ -22,10 +24,10 @@ echo "NG_APP_TENANT_ID=$tenantId" > client/.env
 echo "NG_APP_CLIENT_ID=$spaClientId" >> client/.env
 echo "NG_APP_API_CLIENT_ID=$apiClientId" >> client/.env
 
+
 pip install -r requirements.txt
 
 cd server && mvn clean install && cd ..
 cd client && npm install && cd ..
 
 playwright install --with-deps chromium
-
